d2fb6c4e02d8a6d6e9fc12dd21674ad169dfe2b9
Edward Nguyen <monokaijs@gmail.com>
2023-05-25 10:09:34 +0700

    :rocket: fix get data method

--- src/pages/api/[videoId].js
+++ src/pages/api/[videoId].js
@@ -1,7 +1,13 @@
 import ytdl from "@distube/ytdl-core";
+import NextCors from 'nextjs-cors';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { videoId } = req.query;
+  await NextCors(req, res, {
+    methods: ['GET'],
+    origin: '*',
+    optionsSuccessStatus: 200,
+  });
   ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`).then((info) => {
     res.status(200).json(info.videoDetails);
   });
