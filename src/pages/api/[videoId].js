e131870d7dcc52dec9c7c8dc7a389c6faf526b0c
Edward Nguyen <monokaijs@gmail.com>
2023-05-25 10:07:41 +0700

    :rocket: fix get data method

--- src/pages/api/[videoId].js
+++ src/pages/api/[videoId].js
@@ -2,7 +2,7 @@ import ytdl from "@distube/ytdl-core";
 
 export default function handler(req, res) {
   const { videoId } = req.query;
-  ytdl.video(`https://www.youtube.com/watch?v=${videoId}`).then((info) => {
+  ytdl.getInfo(`https://www.youtube.com/watch?v=${videoId}`).then((info) => {
     res.status(200).json(info.videoDetails);
   });
 }
